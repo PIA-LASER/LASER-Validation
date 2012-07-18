da3350d338351525e88115b5f0f081501721c0d6
Dominik Huebner <contact@dhuebner.com>
2012-07-18 20:22:17 +0200

    adapted nlinks

--- nlinks.rb
+++ nlinks.rb
@@ -2,37 +2,65 @@ require "redis"
 
 threshold = 0
 
+f = File.open("/Users/dominik/Desktop/maaaaan", "r")
+recs = {}
+
+f.each_line do|line|
+  prefs = line.strip!.split(",")
+
+  user_id = prefs[0].to_i
+  item_id = prefs[1].to_i
+  pref = prefs[2].to_f
+
+  if(recs[user_id].nil?)
+    recs[user_id] = {item_id => pref}
+  else
+    recs[user_id][item_id] = pref
+  end
+end
+f.close
+
+f = File.open("/Users/dominik/Desktop/maaaaan2", "r")
+links = {}
+
+f.each_line do|line|
+  line = line.strip!.split(",")
+
+  user_id = line[0].to_i
+  item_id = line[1].to_i
+  pref = line[2].to_f
+
+  if(links[user_id].nil?)
+    links[user_id] = {item_id => pref}
+  else
+    links[user_id][item_id] = pref
+  end
+end
+f.close
+
 ARGV.each do |arg|
 	threshold = arg.to_i
-end
-
-con = Redis.new(:host => "master", :db => 0)
-
-users = con.keys('users.*')
-
-users = users.map do |user|
-	user.gsub("users.","")
 end
 
 total_filtered = 0
 total_raw = 0
 
-users.each do |id|
-	rec = con.zrange("users."+id,0,-1, withscores: true)
-	links = con.lrange("user."+id+".links",0,-1)	
+recs.each_pair do |user_id, pref|
+  recommendation_count = recs[user_id].count
+  link_count = links[user_id].count
 
 	rec_filtered = []	
 
-	rec.each do |pref|
-		if pref[1].to_f > threshold
-			rec_filtered << pref
-		end	
-	end	
+  pref.each_pair do |item_id, pref_val|
+    if pref_val > threshold
+      rec_filtered << pref_val
+    end
+  end
 	
-	total_raw += (rec.count.to_f / links.count.to_f)
-	total_filtered += (rec_filtered.count.to_f / links.count.to_f)
+	total_raw += (recommendation_count.to_f / link_count.to_f)
+	total_filtered += (rec_filtered.count.to_f / link_count.to_f)
 end
 
 puts "Ratio of number of recommended items and already known items. (recommended/known). Averaged over all users."
-puts "unfiltered: " + (total_raw / users.count).to_s
-puts "filtered (threshold: " + threshold.to_s + "): " + (total_filtered / users.count).to_s
+puts "unfiltered: " + (total_raw / links.count).to_s
+puts "filtered (threshold: " + threshold.to_s + "): " + (total_filtered / links.count).to_s
